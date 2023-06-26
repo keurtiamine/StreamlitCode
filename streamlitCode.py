3ab2f20a2fbf177ba37d71aef9e06763676a3aad
unknown <aminekeurti99@gmail.com>
2023-06-26 18:22:28 +0100

    Modifications code

--- streamlitCode.py
+++ streamlitCode.py
@@ -1,15 +1,10 @@
 import streamlit as st
-from pdf2image import convert_from_path
 
 # Titre de l'interface graphique
-st.title("Affichage d'un PDF")
+st.title("Affichage intégral de la boucle eau-vapeur")
 
-# Chemin vers le fichier PDF
-pdf_path = "SCHEMA BOUCLE EAU VAPEUR.pdf"
+# Chemin vers l'image
+image_path = "SCHEMA BOUCLE EAU VAPEUR.jpg"
 
-# Conversion du PDF en images
-images = convert_from_path(pdf_path)
-
-# Affichage des images
-for i, image in enumerate(images):
-    st.image(image, caption=f"Page {i+1}", use_column_width=True)
+# Affichage de l'image
+st.image(image_path, caption='Image', use_column_width=True)
