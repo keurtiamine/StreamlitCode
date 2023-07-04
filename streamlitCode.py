5cfbbb25c0451e32a1e11534cef34b99e54ed2fa
keurtiamine <56561013+keurtiamine@users.noreply.github.com>
2023-07-04 22:49:56 +0100

    Update streamlitCode.py

--- streamlitCode.py
+++ streamlitCode.py
@@ -49,8 +49,13 @@ image_path = "SCHEMA BOUCLE EAU VAPEUR.jpg"
 
 # Affichage de l'image
 st.image(image_path, caption='Image', use_column_width=True)
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 # Fonction pour calculer le KPI en fonction des paramètres sélectionnés
-def calculate_kpi(param1, param2, norm):
+def calculate_kpi(param1, param2, norm_lower, norm_upper):
     # Test pour l'opération de division
     if isinstance(param1, str) or isinstance(param2, str):
         st.write("Les paramètres ne doivent pas être des chaînes de caractères")
@@ -59,10 +64,11 @@ def calculate_kpi(param1, param2, norm):
     # Logique de calcul du KPI en utilisant les paramètres sélectionnés
     kpi_result = param1 / param2
     
-    # Calcul de la différence avec la norme
-    diff = np.abs(kpi_result - norm)
+    # Calcul de la différence avec les normes
+    diff_lower = np.abs(kpi_result - norm_lower)
+    diff_upper = np.abs(kpi_result - norm_upper)
     
-    return kpi_result, diff
+    return kpi_result, diff_lower, diff_upper
 
 # Interface utilisateur
 st.title('Calcul du KPI')
@@ -114,8 +120,9 @@ if tableau1_file is not None and tableau2_file is not None:
     # Sélection des paramètres pour le deuxième paramètre
     selected_param2 = st.selectbox('Choisir un paramètre pour le deuxième paramètre', param2_columns)
     
-    # Demande de la norme du KPI
-    norm = st.number_input("Norme du KPI", value=1.0)
+    # Demande de la norme inférieure et supérieure du KPI
+    norm_lower = st.number_input("Norme inférieure du KPI", value=0.0)
+    norm_upper = st.number_input("Norme supérieure du KPI", value=1.0)
     
     # Bouton de calcul
     calculate_button = st.button('Calculer')
@@ -126,26 +133,28 @@ if tableau1_file is not None and tableau2_file is not None:
         param1_values = param1_values[selected_param1].tolist()
         param2_values = param2_values[selected_param2].tolist()
         
-        # Calcul du KPI et différence avec la norme
-        results = [calculate_kpi(param1, param2, norm) for param1, param2 in zip(param1_values, param2_values)]
+        # Calcul du KPI et différences avec les normes
+        results = [calculate_kpi(param1, param2, norm_lower, norm_upper) for param1, param2 in zip(param1_values, param2_values)]
         
         # Filtrage des résultats valides
-        valid_results = [(kpi, diff) for kpi, diff in results if kpi is not None]
-        kpi_values, diff_values = zip(*valid_results)
+        valid_results = [(kpi, diff_lower, diff_upper) for kpi, diff_lower, diff_upper in results if kpi is not None]
+        kpi_values, diff_lower_values, diff_upper_values = zip(*valid_results)
         
         # Affichage du tableau de résultats
         st.write('Résultats du KPI :')
-        kpi_df = pd.DataFrame({selected_param1: param1_values, selected_param2: param2_values, 'KPI': kpi_values, 'Différence': diff_values})
+        kpi_df = pd.DataFrame({selected_param1: param1_values, selected_param2: param2_values, 'KPI': kpi_values, 'Différence inférieure': diff_lower_values, 'Différence supérieure': diff_upper_values})
         st.write(kpi_df)
         
         # Affichage du graphe
         st.write('Graphe du KPI :')
         plt.figure(figsize=(10, 6))
         plt.plot(np.arange(1, 13), kpi_values, marker='o', linestyle='-', linewidth=2, color='blue', label='KPI')
-        plt.axhline(norm, color='red', linestyle='--', linewidth=2, label='Norme')
+        plt.axhline(norm_lower, color='red', linestyle='--', linewidth=2, label='Norme inférieure')
+        plt.axhline(norm_upper, color='green', linestyle='--', linewidth=2, label='Norme supérieure')
         plt.xlabel('Index')
         plt.ylabel('KPI')
-        plt.title('Évolution du KPI avec la Norme')
+        plt.title('Évolution du KPI avec les Normes')
         plt.legend()
         plt.grid(True)
         st.pyplot(plt)
+
