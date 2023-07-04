7461ec28cdb924df68d6e1c144ecc1dd28ac9260
keurtiamine <56561013+keurtiamine@users.noreply.github.com>
2023-07-04 21:42:37 +0100

    Test code KPI personnalisés seulement

--- streamlitCode.py
+++ streamlitCode.py
@@ -3,16 +3,20 @@ import streamlit as st
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-#style use the entire screen width
+# Stockage des tableaux Excel chargés par l'utilisateur
+tableau_previsionel = None
+energie_electrique = None
+production_vapeur = None
+
+# Style utilisant toute la largeur de l'écran
 st.set_page_config(layout="wide")
 st.markdown(
     """
     <style>
-   
     body {
-        background-color: coral; /* Replace #f0f0f0 with your desired background color */
+        background-color: coral; /* Remplacez #f0f0f0 par la couleur d'arrière-plan souhaitée */
     }
-    
+
     /* Modifier la police et la taille du titre */
     .title {
         font-family: 'Arial', sans-serif;
@@ -43,7 +47,6 @@ st.markdown(
 # Titre de l'interface graphique
 st.markdown("<h1 style='text-align: center;'>Interface graphique PMP</h1>", unsafe_allow_html=True)
 
-
 st.title("Affichage intégral de la boucle eau-vapeur")
 
 # Chemin vers l'image
@@ -61,9 +64,10 @@ bilan_thermique = pd.read_excel(excel_path)
 # Affichage des données dans Streamlit
 st.title("Base de données des composantes du cycle eau-vapeur")
 st.dataframe(bilan_thermique)
+
 #############################################################################################
 
-##tableau previsionel
+## Tableau prévisionnel
 st.title("Tableau prévisionnel")
 tableau_previsionel_file = st.file_uploader("Upload Excel file", type=["xlsx"], key=1)
 if tableau_previsionel_file is not None:
@@ -71,9 +75,8 @@ if tableau_previsionel_file is not None:
     st.write("File uploaded successfully.")
     st.write("File Name:", tableau_previsionel_file.name)
     st.dataframe(tableau_previsionel)
-    
 
-## tableau energie electrique
+## Tableau énergie électrique
 st.title("Production de l'énergie électrique")
 energie_electrique_file = st.file_uploader("Upload Excel file", type=["xlsx"], key=2)
 if energie_electrique_file is not None:
@@ -81,167 +84,35 @@ if energie_electrique_file is not None:
     st.write("File uploaded successfully.")
     st.write("File Name:", energie_electrique_file.name)
     st.dataframe(energie_electrique)
-st.title("Production de la vapeur HP")
+
+## Tableau production de vapeur
+st.title("Production de la vapeur")
 production_vapeur_file = st.file_uploader("Upload Excel file", type=["xlsx"], key=3)
 if production_vapeur_file is not None:
-    production_vapeur = pd.read_excel(production_vapeur_file, header=[0, 1])
+    production_vapeur = pd.read_excel(production_vapeur_file)
     st.write("File uploaded successfully.")
     st.write("File Name:", production_vapeur_file.name)
     st.dataframe(production_vapeur)
 
+# Fonction pour le calcul des KPI
 def kpi():
-    st.write("Ces tableaux ont pour but le calcul des différents KPI suivants :")
-    st.markdown("1- Capacité de production inutilisée")
-    # afficher la capacité de production inutilisée pour chaque mois
+    if tableau_previsionel is not None and energie_electrique is not None and production_vapeur is not None:
+        # Effectuer le calcul des KPI ici en utilisant les tableaux
+        # tableau_previsionel, energie_electrique, production_vapeur
+        # ...
 
-    # creer une data frame vide
-    capacite_production_inutilisee = pd.DataFrame()
-    capacite_max = bilan_thermique[(bilan_thermique['Unité / Equipement'] == 'SAP') & (
-                bilan_thermique['Type de Fuide'] == 'Vapeur HP')]['Débit (T/H)'].min() * 24 * 30
+        # Exemple : Calcul du coefficient de corrélation entre la production de vapeur et l'énergie électrique
+        df = pd.merge(production_vapeur, energie_electrique, on='Date')
+        x = df['Production Vapeur']
+        y = df['Energie Electrique']
+        model = LinearRegression()
+        model.fit(x.values.reshape(-1, 1), y.values.reshape(-1, 1))
+        correlation = model.coef_[0][0]
 
-    st.write("La capacité maximale de production de vapeur HP est de : ", capacite_max, "T/Mois")
-    # fill the data frame capacite_productio_inutilisee with the values month and capacitemax-capacitemonth from production_vapeur
-    capacite_production_inutilisee['Mois'] = production_vapeur['Mois']
-    capacite_production_inutilisee['Capacité de production inutilisée'] = capacite_max - production_vapeur['Prod\nSAP']
-    st.write("capacite_production_inutilisee['Capacité de production inutilisée'] = capacite_max - production_vapeur['Prod\nSAP']")
-    capacite_production_inutilisee['pourcentage inutilisé'] = (capacite_production_inutilisee[
-                                                                   'Capacité de production inutilisée'] / capacite_max) * 100
-    capacite_production_inutilisee["Nombre d'heure de pannes machine/ maintenance"] = capacite_production_inutilisee[
-                                                                                          'Capacité de production inutilisée'] / capacite_max * 720
+        # Affichage du résultat
+        st.title("Résultats des KPI")
+        st.write("Coefficient de corrélation entre la production de vapeur et l'énergie électrique:", correlation)
 
-    capacite_production_inutilisee.loc[capacite_production_inutilisee[
-                                           "Nombre d'heure de pannes machine/ maintenance"] < 0, "Nombre d'heure de pannes machine/ maintenance"] = 0
-
-    st.dataframe(capacite_production_inutilisee.iloc[1:13, :])
-
-    st.markdown("2- Fiabilité des planings et des prévisions")
-
-    # creer une data frame pour comparer la df tableau_previsionel et la dfenergie_electrique
-    fiabilite_prevision = pd.DataFrame()
-    fiabilite_prevision['Mois'] = tableau_previsionel['Mois']
-    fiabilite_prevision['Prévision (MWH)'] = tableau_previsionel['Production\nPrévue (MWH)\nGTA']
-    fiabilite_prevision['Production réalisée (MWH)'] = energie_electrique['Production\nRéalisée (MWH)\nGTA']
-    fiabilite_prevision['Rapport de Fiabilité Réalisée/Prévision'] = (fiabilite_prevision['Production réalisée (MWH)'] /
-                                                                      fiabilite_prevision['Prévision (MWH)']) * 100
-    fiabilite_prevision['Différence'] = fiabilite_prevision['Production réalisée (MWH)'] - fiabilite_prevision[
-        'Prévision (MWH)']
-
-    # afficher la df fiabilite_prevision dqns un tableau qui prend la largeur de l'ecran
-
-    st.dataframe(fiabilite_prevision.iloc[1:13, :])
-    st.write("La fiabilité des prévisions est de : ",
-             fiabilite_prevision['Rapport de Fiabilité Réalisée/Prévision'].iloc[1: 13].mean(), "%")
-    st.line_chart(fiabilite_prevision['Rapport de Fiabilité Réalisée/Prévision'][:13])
-
-
-
-
-    st.markdown("3- Rendement de la production d'énergie électrique")
-    # Énergie thermique = Masse de vapeur (en kg) × Chaleur spécifique de la vapeur (en J/kg·°C) × Écart de température (en °C)
-    energie1 = (141 - 53) * 1000 * 1440 * (500 - 211)
-    energie2 = (141 - 11.8) * 1000 * 1440 * (500 - 118)
-    energie3 = (141 - 13.2) * 1000 * 1440 * (500 - 80)
-    energie4 = (141 - 70) * 1000 * 1440 * (500 - 33.5)
-    energieTotal = energie1 + energie2 + energie3 + energie4
-    # Rendement de la production d'électricité = (Électricité produite / Énergie thermique de la vapeur HP) * 100
-
-    rendement_production_electrique = pd.DataFrame()
-    rendement_production_electrique['Mois'] = energie_electrique['Mois']
-    rendement_production_electrique['Rendement de la production d\'énergie électrique'] = ((energie_electrique[
-                                                                                                'Production\nRéalisée (MWH)\nGTA'] * 3.6 * (
-                                                                                                        10 ** 9)) / (
-                                                                                                       720 * energieTotal)) * 100
-    st.write("rendement_production_electrique['Rendement de la production d'énergie électrique'] = ((energie_electrique'Production Réalisée (MWH) GTA'] * 3.6 * (10 ** 9)) / (720 * energieTotal)) * 100")
-    st.dataframe(rendement_production_electrique.iloc[1:13, :])
-    st.write("Le rendement de la production d'énergie électrique est de : ",
-             rendement_production_electrique['Rendement de la production d\'énergie électrique'].iloc[1: 13].mean(),
-             "%")
-    st.line_chart(rendement_production_electrique['Rendement de la production d\'énergie électrique'][:13])
-
-    st.markdown("4- Taux de rejet de vapeur HP")
-    # Taux de rejet de vapeur HP = (Vapeur HP rejetée / Vapeur HP produite) * 100
-    taux_rejet_vapeur = pd.DataFrame()
-    taux_rejet_vapeur['Mois'] = production_vapeur['Mois']
-    consomationDetente = production_vapeur['Consommation']['Détente']
-    productionSap = production_vapeur['Prod\nSAP']['Unnamed: 1_level_1']
-    taux_rejet_vapeur['Taux de rejet de vapeur HP'] = ((consomationDetente) / productionSap) * 100
-    st.write("taux_rejet_vapeur['Taux de rejet de vapeur HP'] = ((consomationDetente) / productionSap) * 100")
-    st.dataframe(taux_rejet_vapeur.iloc[1:13, :])
-    st.write("Le taux de rejet de vapeur HP est de : ",
-             taux_rejet_vapeur['Taux de rejet de vapeur HP'].iloc[1: 13].mean(), "%")
-    st.line_chart(taux_rejet_vapeur['Taux de rejet de vapeur HP'][:13])
-
-    st.markdown("5- Taux de productivité de l'énergie électrique")
-    # Taux de productivité de l'énergie électrique = (Énergie électrique produite / Énergie consommé) * 100
-    taux_productivite_electrique = pd.DataFrame()
-    taux_productivite_electrique['Mois'] = energie_electrique['Mois']
-    taux_productivite_electrique['Taux de productivité de l\'énergie électrique'] = energie_electrique[
-                                                                                        'Production\nRéalisée (MWH)\nGTA'] / \
-                                                                                    energie_electrique[
-                                                                                        'Conso\nUsine'] * 100
-    st.write("taux_productivite_electrique['Taux de productivité de l\'énergie électrique'] = energie_electrique'Production Réalisée (MWH) GTA'] / energie_electrique'Conso Usine'] * 100")
-    st.dataframe(taux_productivite_electrique.iloc[1:13, :])
-    st.write("Le taux de productivité de l'énergie électrique est de : ",
-             taux_productivite_electrique['Taux de productivité de l\'énergie électrique'].iloc[1: 13].mean(), "%")
-    st.line_chart(taux_productivite_electrique['Taux de productivité de l\'énergie électrique'][:13])
-# Fonction pour calculer le KPI en fonction des paramètres sélectionnés
-def calculate_kpi(param1, param2):
-    # Logique de calcul du KPI en utilisant les paramètres sélectionnés
-    kpi_result = param1 * param2  # Exemple de calcul simple
-    
-    return kpi_result
-
-# Interface utilisateur
-st.title('Calcul du KPI')
-
-# Définition des tableaux de données
-tableau1 = pd.DataFrame({'Paramètre 1': [10, 20, 30], 'Paramètre 2': [1.5, 2.5, 3.5]})
-tableau2 = pd.DataFrame({'Paramètre A': [5, 10, 15], 'Paramètre B': [2, 4, 6]})
-
-# Liste des tableaux disponibles avec leurs noms
-tableaux = {'Tableau 1': tableau1, 'Tableau 2': tableau2}
-
-# Sélection du tableau
-tableau_choice = st.selectbox('Choisir un tableau', list(tableaux.keys()))
-
-# Récupération du tableau sélectionné
-selected_tableau = tableaux[tableau_choice]
-
-# Affichage du tableau
-st.write('Tableau sélectionné :')
-st.write(selected_tableau)
-
-# Récupération des noms des colonnes (paramètres) du tableau sélectionné
-params = selected_tableau.columns.tolist()
-
-# Sélection des paramètres
-selected_params = st.multiselect('Choisir les paramètres', params)
-
-# Bouton de calcul
-calculate_button = st.button('Calculer')
-
-# Vérification si le bouton de calcul a été cliqué
-if calculate_button:
-    # Récupération des valeurs des paramètres sélectionnés
-    param_values = []
-    for param in selected_params:
-        param_value = st.number_input(param, value=0.0)
-        param_values.append(param_value)
-    
-    # Calcul du KPI
-    kpi_value = calculate_kpi(*param_values)
-    
-    # Affichage du tableau de résultats
-    st.write('Résultats du KPI :')
-    kpi_df = pd.DataFrame({param: [value] for param, value in zip(selected_params, param_values)})
-    kpi_df['KPI'] = kpi_value
-    st.write(kpi_df)
-    
-    # Affichage du graphe
-    st.write('Graphe du KPI :')
-    plt.plot(kpi_df['KPI'])
-    plt.xlabel('Index')
-    plt.ylabel('KPI')
-    st.pyplot(plt)
-if production_vapeur_file is not None and energie_electrique_file is not None and tableau_previsionel_file is not None:
+# Bouton pour le calcul des KPI
+if st.button("Calculer les KPI"):
     kpi()
