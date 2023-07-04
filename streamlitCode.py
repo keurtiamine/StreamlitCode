8d8adffd74d8a76defb95ce85cdc036389321734
keurtiamine <56561013+keurtiamine@users.noreply.github.com>
2023-07-04 21:29:18 +0100

    Code d'origine

--- streamlitCode.py
+++ streamlitCode.py
@@ -184,64 +184,6 @@ def kpi():
     st.write("Le taux de productivité de l'énergie électrique est de : ",
              taux_productivite_electrique['Taux de productivité de l\'énergie électrique'].iloc[1: 13].mean(), "%")
     st.line_chart(taux_productivite_electrique['Taux de productivité de l\'énergie électrique'][:13])
-# Fonction pour calculer le KPI en fonction des paramètres sélectionnés
-def calculate_kpi(param1, param2):
-    # Logique de calcul du KPI en utilisant les paramètres sélectionnés
-    kpi_result = param1 * param2  # Exemple de calcul simple
-    
-    return kpi_result
 
-# Interface utilisateur
-st.title('Calcul du KPI')
-
-# Définition des tableaux de données
-tableau1 = pd.DataFrame({'Paramètre 1': [10, 20, 30], 'Paramètre 2': [1.5, 2.5, 3.5]})
-tableau2 = pd.DataFrame({'Paramètre A': [5, 10, 15], 'Paramètre B': [2, 4, 6]})
-
-# Liste des tableaux disponibles avec leurs noms
-tableaux = {'Tableau 1': tableau1, 'Tableau 2': tableau2}
-
-# Sélection du tableau
-tableau_choice = st.selectbox('Choisir un tableau', list(tableaux.keys()))
-
-# Récupération du tableau sélectionné
-selected_tableau = tableaux[tableau_choice]
-
-# Affichage du tableau
-st.write('Tableau sélectionné :')
-st.write(selected_tableau)
-
-# Récupération des noms des colonnes (paramètres) du tableau sélectionné
-params = selected_tableau.columns.tolist()
-
-# Sélection des paramètres
-selected_params = st.multiselect('Choisir les paramètres', params)
-
-# Bouton de calcul
-calculate_button = st.button('Calculer')
-
-# Vérification si le bouton de calcul a été cliqué
-if calculate_button:
-    # Récupération des valeurs des paramètres sélectionnés
-    param_values = []
-    for param in selected_params:
-        param_value = st.number_input(param, value=0.0)
-        param_values.append(param_value)
-    
-    # Calcul du KPI
-    kpi_value = calculate_kpi(*param_values)
-    
-    # Affichage du tableau de résultats
-    st.write('Résultats du KPI :')
-    kpi_df = pd.DataFrame({param: [value] for param, value in zip(selected_params, param_values)})
-    kpi_df['KPI'] = kpi_value
-    st.write(kpi_df)
-    
-    # Affichage du graphe
-    st.write('Graphe du KPI :')
-    plt.plot(kpi_df['KPI'])
-    plt.xlabel('Index')
-    plt.ylabel('KPI')
-    st.pyplot(plt)
 if production_vapeur_file is not None and energie_electrique_file is not None and tableau_previsionel_file is not None:
     kpi()
