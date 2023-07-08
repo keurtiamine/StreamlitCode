a3077447714dfeff28aa7201c31c464096fc3dbc
Marouane <marouanenili123@gmail.com>
2023-07-08 18:48:40 +0200

    excel download button:

--- streamlitCode.py
+++ streamlitCode.py
@@ -145,7 +145,7 @@ if tableau1_file is not None and tableau2_file is not None:
         kpi_values, diff_lower_values, diff_upper_values = zip(*valid_results)
         
         # Affichage du tableau de résultats
-        st.write('Résultats du KPI :')
+        var  = st.write('Résultats du KPI :')
         kpi_df = pd.DataFrame({selected_param1: param1_values, selected_param2: param2_values, 'KPI': kpi_values, 'Différence inférieure': diff_lower_values, 'Différence supérieure': diff_upper_values})
         st.write(kpi_df)
         excel =  kpi_df.to_excel("kpi.xlsx")
